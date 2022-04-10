e436b0a0bd3c6a9dbc37d191d2bf71d7b407f1f0
jdmattern <jdmattern@gmail.com>
2022-04-10 14:29:44 -0500

    started GetListOfLocations stream class

--- airbyte-integrations/connectors/source-hydrovu/source_hydrovu/source.py
+++ airbyte-integrations/connectors/source-hydrovu/source_hydrovu/source.py
@@ -5,9 +5,17 @@ from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
+
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import BackendApplicationClient
 
+
+from abc import ABC
+
+# abstractmethod may be needed at a later time
+#from abc import ABC, abstactmethod
 
 from . import pokemon_list
 
@@ -15,23 +23,31 @@ from . import pokemon_list
 class SourceHydrovu(AbstractSource):
     
     def check_connection(self, logger, config) -> Tuple[bool, any]:
-        print ('config["client_secret"]')
-        print (config["client_secret"])
-        print ('config["account_id"]')
-        print (config["account_id"])
+        #print ('config["client_secret"]')
+        #print (config["client_secret"])
+        #print ('config["account_id"]')
+        #print (config["account_id"])
         input_pokemon = config["pokemon_name"]
 
-        # create session
-        client = BackendApplicationClient(client_id=config["account_id"])
-        oauth = OAuth2Session(client=client)
-        token = oauth.fetch_token(token_url='https://www.hydrovu.com/public-api/oauth/token', client_id=config["account_id"], client_secret=config["client_secret"])
 
-        # get list of locations
-        r = oauth.get('https://www.hydrovu.com/public-api/v1/locations/list')
-        locations = r.json()
+        try:
 
-        print ("locations")
-        print (locations)
+            # create session
+            client = BackendApplicationClient(client_id=config["account_id"])
+            oauth = OAuth2Session(client=client)
+            token = oauth.fetch_token(token_url='https://www.hydrovu.com/public-api/oauth/token', client_id=config["account_id"], client_secret=config["client_secret"])
+
+            # get list of locations
+            #r = oauth.get('https://www.hydrovu.com/public-api/v1/locations/list')
+            #locations = r.json()
+
+            #print ("locations")
+            #print (locations)
+
+            return True, None
+
+        except Exception as e:
+            return False, e
 
 
 
@@ -66,7 +82,87 @@ class SourceHydrovu(AbstractSource):
 
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        return [Pokemon(pokemon_name=config["pokemon_name"])]
+        '''
+           The streams method defines what our source can output
+        '''
+        # create session
+        client = BackendApplicationClient(client_id=config["account_id"])
+        oauth = OAuth2Session(client=client)
+        token = oauth.fetch_token(token_url='https://www.hydrovu.com/public-api/oauth/token', client_id=config["account_id"], client_secret=config["client_secret"])
+
+        #authenticator = TokenAuthenticator(config["client_secret"])
+        #args = {"authenticator": authenticator, "account_id": config["account_id"]}
+        #args = {"account_id": config["account_id"]}
+        #args = {"authenticator": authenticator}
+
+        # We might need below if specifying a start_date for the time series data
+        #args = {"authenticator": authenticator, "account_id": config["account_id"], "start_date": start_date}
+
+        return [
+            #Pokemon(pokemon_name=config["pokemon_name"]),
+            #GetListOfLocations(pokemon_name=config["pokemon_name"]),
+            #GetListOfLocations(**args),
+            #GetListOfLocations(pokemon_name=config["pokemon_name"], **args),
+            
+            #GetListOfLocations(**args),
+            GetListOfLocations(oauth),
+        ]
+
+
+#class GetListOfLocations(HttpStream, ABC):
+class GetListOfLocations(HttpStream):
+    url_base = "https://pokeapi.co/api/v2/"
+
+    # Set this as a noop.
+    primary_key = None
+
+    #def __init__(self, pokemon_name: str, **kwargs):
+    #    super().__init__(**kwargs)
+    #    # Here's where we set the variable from our input to pass it down to the source.
+    #    self.pokemon_name = pokemon_name
+            
+    #def __init__(self, pokemon_name: str, **kwargs):
+    def __init__(self, oauth):
+
+        # get list of locations
+        r = oauth.get('https://www.hydrovu.com/public-api/v1/locations/list')
+        locations = r.json()
+
+        print ("locations")
+        print (locations)
+
+
+    def path(self, **kwargs) -> str:
+        pokemon_name = self.pokemon_name
+        # This defines the path to the endpoint that we want to hit.
+        return f"pokemon/{pokemon_name}"
+
+    def request_params(
+            self,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> MutableMapping[str, Any]:
+        # The api requires that we include the Pokemon name as a query param so we do that in this method.
+        return {"pokemon_name": self.pokemon_name}
+
+    def parse_response(
+            self,
+            response: requests.Response,
+            stream_state: Mapping[str, Any],
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        # The response is a simple JSON whose schema matches our stream's schema exactly,
+        # so we just return a list containing the response.
+        return [response.json()]
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        # While the PokeAPI does offer pagination, we will only ever retrieve one Pokemon with this implementation,
+        # so we just return None to indicate that there will never be any more pages in the response.
+        return None
+
+
 
 
 class Pokemon(HttpStream):
